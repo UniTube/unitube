be3718c265887caf669d6a7fc6b1d46a1248d025
sidrasali <s928309@email.com>
2026-06-29 12:45:44 +0200

    fix link issue in VideoCard

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-bbbdf4d3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2m8v9kobmbg"
+    "revision": "0.a8b7p8rhu6g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
